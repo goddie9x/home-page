3c67dbe632f87e882db79b1a3a246a40a392a9f6
goddie9x <goddie9x@yahoo.com>
2021-03-17 21:11:20 +0700

    update video

--- assets/scripts/main_script.js
+++ assets/scripts/main_script.js
@@ -1,115 +1,55 @@
-var
-    funnyTextElement = document.querySelector('.headingSection .funnyText'),
-    audioImYours = document.getElementById('ImYours'),
-    Copyright = document.querySelector('.copyright'),
-    isClickOddFunnyElement = true,
-    isClickOddCopyright = true;
-
-
-
-var listTextFun = function(numberRandom) {
-    switch (numberRandom) {
-        case 0:
-            {
-                return 'đừng chạm vào đây *.*'
-            }
-
-        case 1:
-            {
-                return 'á á á ><'
-            }
-
-        case 2:
-            {
-                return 'đố anh bắt được iem :3'
-            }
-
-        case 3:
-            {
-                return 'em đùa đấy đừng chạm vào em @@'
-            }
-
-        case 4:
-            {
-                return 'đã bảo đừng động vào tôi rồi ~~'
-            }
-
-        case 5:
-            {
-                return 'tránh ra nào $.$'
-            }
-
-        case 6:
-            {
-                return 'ơ hay 0.o'
-            }
-
-        case 7:
-            {
-                return 'né né né -.-'
-            }
-
-        case 8:
-            {
-                return 'click vào copyright để dừng lại :v'
-            }
-
-
-    }
-}
-
-
 setInterval(function() {
     let H1Element = document.querySelector('.headingTitle');
     H1Element.classList.toggle('colorChangeBlack');
-}, 300);
+}, 500);
 
+function elementMoveWhenMouseMove(object) {
+    let funnyTextElement = document.querySelector(object.funnyTextElement),
+        arrayTextRandom = object.arrayTextRandom;
+    if (arrayTextRandom) {
+        let arrayLength = arrayTextRandom.length;
 
-
-funnyTextElement.onmousemove = function(e) {
-
-    let randomLeft = `${Math.floor(Math.random()*95)}vw`,
-        randomTop = `${Math.floor(Math.random()*95)}vh`,
-        randomText = Math.floor(Math.random() * 9);
-
-    Object.assign(e.target.style, {
-        left: randomLeft,
-        top: randomTop,
-    });
-    e.target.innerText = listTextFun(randomText);
-}
-
-funnyTextElement.onclick = function(e) {
-    if (isClickOddFunnyElement) {
-        audioImYours.style.display = 'block';
-        isClickOddFunnyElement = false;
-    } else {
-        audioImYours.style.display = 'none';
-        isClickOddFunnyElement = true;
-    }
-
-    funnyTextElement.textContent = 'ựa 0.o'
-}
-
-Copyright.onclick = function(e) {
-    if (isClickOddCopyright) {
-        funnyTextElement.onmousemove = function() {};
-        isClickOddCopyright = false;
-        Copyright.classList.add('buttonF');
-    } else {
         funnyTextElement.onmousemove = function(e) {
+
             let randomLeft = `${Math.floor(Math.random()*95)}vw`,
                 randomTop = `${Math.floor(Math.random()*95)}vh`,
-                randomText = Math.floor(Math.random() * 9);
+                randomText = Math.floor(Math.random() * arrayLength);
 
             Object.assign(e.target.style, {
                 left: randomLeft,
                 top: randomTop,
             });
-            e.target.innerText = listTextFun(randomText);
+            e.target.innerText = arrayTextRandom[randomText];
         }
-        isClickOddCopyright = true;
-        Copyright.classList.remove('buttonF');
+    } else {
+        funnyTextElement.onmousemove = function() {};
+    }
+
+}
+
+let Copyright = document.querySelector('.copyright');
+
+Copyright.onclick = function(e) {
+    isClick = Copyright.classList.contains('buttonF');
+    if (isClick) {
+        elementMoveWhenMouseMove({
+            funnyTextElement: '.funnyText',
+            arrayTextRandom: ['đừng chạm vào đây *.*',
+                'á á á ><',
+                'đố anh bắt được iem :3',
+                'em đùa đấy đừng chạm vào em @@',
+                'đã bảo đừng động vào tôi rồi ~~',
+                'tránh ra nào $.$',
+                'ơ hay 0.o', 'né né né -.-',
+                'click vào copyright để dừng lại :v'
+            ]
+        });
+        Copyright.classList.toggle('buttonF');
+    } else {
+        elementMoveWhenMouseMove({
+            funnyTextElement: '.funnyText'
+        });
+        Copyright.classList.toggle('buttonF');
     }
 }
 
@@ -149,16 +89,28 @@ function ShowDivSelect(object) {
 
 //need style display=none hard code on html
 
-function ShowHideDiv(object) {
+function ShowHideChangeDiv(object) {
     let elementActive = document.querySelector(object.elementActive),
-        elementShowHide = document.querySelector(object.elementShowHide);
+        ArrayElementShowHide = object.ArrayElementShowHide,
+        elementNeedChangeHTML = object.elementNeedChangeHTML,
+        HTMLchange = object.HTMLchange;
 
     elementActive.onclick = function() {
-        if (elementShowHide.style.display == 'none') {
-            elementShowHide.style.display = 'inline-block';
+        ArrayElementShowHide.forEach(
+            function(text) {
+                elementShowHide = document.querySelector(text);
 
-        } else {
-            elementShowHide.style.display = 'none';
+                if (elementShowHide.style.display == 'none') {
+                    elementShowHide.style.display = 'inline-block';
+
+                } else {
+                    elementShowHide.style.display = 'none';
+                }
+            }
+        );
+        if (elementNeedChangeHTML && HTMLchange) {
+            let target = document.querySelector(elementNeedChangeHTML);
+            target.innerHTML = HTMLchange;
         }
     }
 }
@@ -174,4 +126,13 @@ function clickOutsideClose(elementName, exceptionElement) {
             elementClickOutHide.style.display = 'none';
         }
     });
+}
+
+function clickedChangeElement(element, elementHtmlInner) {
+    let elementChange = document.querySelector(element);
+    if (elementHtmlInner) {
+        elementChange.onclick = function() {
+            elementChange.innerHTML = elementHtmlInner;
+        }
+    }
 }
